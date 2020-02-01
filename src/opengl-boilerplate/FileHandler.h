afb56bf96df0d19ee55e63db9cbbf990ee375703
William Sandström <williamsandstrom99@hotmail.com>
2020-02-01 18:27:53 +0100

    Fixed an issue with texture coordinates

--- src/opengl-boilerplate/FileHandler.h
+++ src/opengl-boilerplate/FileHandler.h
@@ -91,8 +91,8 @@ public:
 					tinyobj::real_t vx = attrib.vertices[3 * idx.vertex_index + 0];
 					tinyobj::real_t vy = attrib.vertices[3 * idx.vertex_index + 1];
 					tinyobj::real_t vz = attrib.vertices[3 * idx.vertex_index + 2];
-					tinyobj::real_t tx = attrib.texcoords[2 * idx.texcoord_index + 0];
-					tinyobj::real_t ty = attrib.texcoords[2 * idx.texcoord_index + 1];
+					//tinyobj::real_t tx = attrib.texcoords[2 * idx.texcoord_index + 0];
+					//tinyobj::real_t ty = attrib.texcoords[2 * idx.texcoord_index + 1];
 					//vertices.insert(vertices.end(), { vx, vy, vz, tx, ty }); //With textures
 					vertices.insert(vertices.end(), { vx, vy, vz }); //Without textures
 				}
